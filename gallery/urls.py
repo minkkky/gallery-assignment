680f9290c48f71c98dcb7a7c66969815f22081f8
m_ink <mp98dl4@gmail.com>
2022-09-14 16:50:48 +0900

    작가, 작품, 전시 목록 보기 추가

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('arts/', views.ArtListView.as_view(), name='art-list'),
+    path('exbihitions/', views.ExbihitionListView.as_view(), name='exbihition-list'),
+    path('artists/', views.ArtistListView.as_view(), name='artist-list'),
+]
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Art as ArtModel
+from .models import Exbihition as ExbihitionModel
+from user.models import UserInfo as UserInfoModel
 
-# Create your views here.
+
+class ArtListView(ListView):
+    model = ArtModel
+    ordering = '-pk'
+    template_name = 'gallery/arts.html'
+
+
+class ExbihitionListView(ListView):
+    model = ExbihitionModel
+    ordering = 'end'
+    template_name = 'gallery/exbihitions.html'
+
+
+class ArtistListView(ListView):
+    model = UserInfoModel
+    queryset = UserInfoModel.objects.filter(is_artist=True)
+    ordering = '-pk'
+    template_name = 'gallery/artists.html'
+
+
+class SearchResultsView(View):
+    def get(self, request):
+        option = self.request.GET.get("s") # 검색 조건
+        query = self.request.GET.get("q") # 검색어
+
+        if option == '1':
+            results = ArtModel.objects.filter(
+                title__icontains=query
+            )
+
+        elif option == '2':
+            results = UserInfoModel.objects.filter(
+                name__icontains=query
+            )
+
+        elif option == '3':
+            results = ExbihitionModel.objects.filter(
+                title__icontains=query
+            )
+
+        return render(results, 'gallery/search-result.html')
+
+
+class ArtCreateView(CreateView):
+    model = ArtModel
+    fields = ['title', 'price', 'size']
